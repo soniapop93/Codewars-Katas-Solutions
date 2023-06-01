0acd37992bcbeea68a5fa58fe08032bf67e4348a
soniapop93 <popsonia85@gmail.com>
2023-06-01 22:53:26 +0300

    finished codewar challenge disemovowel trolls

---
+++ C#/CodewarsKatas/CodewarsKatas/7-kyu/Disemvowel Trolls/DisemvowelTrolls.cs
@@ -0,0 +1,36 @@
+﻿/* 
+ * LINK: https://www.codewars.com/kata/52fba66badcd10859f00097e/train/csharp
+ * 
+ * Trolls are attacking your comment section!
+ * 
+ * A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+ * 
+ * Your task is to write a function that takes a string and return a new string with all vowels removed.
+ * 
+ * For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+ * 
+ */
+
+using System.Collections.Generic;
+
+namespace CodewarsKatas._7_kyu.Disemvowel_Trolls
+{
+    public class DisemvowelTrolls
+    {
+        public string Disemvowel(string str)
+        {
+            List<string> vowels = new List<string>() { "a", "e", "i", "o", "u"};
+            string newStr = "";
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (!vowels.Contains(str[i].ToString().ToLower()))
+                {
+                    newStr = newStr + str[i];
+                }
+            }
+
+            return newStr;
+        }
+    }
+}
