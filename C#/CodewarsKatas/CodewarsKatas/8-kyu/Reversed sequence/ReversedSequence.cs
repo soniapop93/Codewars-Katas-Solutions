938473eedaf327a05f221cc26c4c85d9a0663690
soniapop93 <popsonia85@gmail.com>
2023-06-05 13:20:19 +0300

    finished challenge reversed sequence

---
+++ C#/CodewarsKatas/CodewarsKatas/8-kyu/Reversed sequence/ReversedSequence.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * LINK: https://www.codewars.com/kata/5a00e05cc374cb34d100000d/train/csharp
+ * 
+ * Build a function that returns an array of integers from n to 1 where n>0.
+ * 
+ * Example : n=5 --> [5,4,3,2,1]
+ */
+
+using System.Collections.Generic;
+
+namespace CodewarsKatas._8_kyu.Reversed_sequence
+{
+    public class ReversedSequence
+    {
+        public static int[] ReverseSeq(int n)
+        {
+            List<int> result = new List<int>();
+            for (int i = n; i > 0; i--)
+            {
+                result.Add(i);
+            }
+            return result.ToArray();
+        }
+    }
+}
