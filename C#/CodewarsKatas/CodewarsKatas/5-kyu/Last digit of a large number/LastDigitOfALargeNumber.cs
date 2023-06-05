f1cd74a04f4c94b6343fc56a108a6703662bf2f5
soniapop93 <popsonia85@gmail.com>
2023-06-05 20:30:38 +0300

    finished challenge last difit of a large number

---
+++ C#/CodewarsKatas/CodewarsKatas/5-kyu/Last digit of a large number/LastDigitOfALargeNumber.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * LINK: https://www.codewars.com/kata/5511b2f550906349a70004e1/train/csharp
+ * 
+ * Define a function that takes in two non-negative integers a and b and returns the last decimal digit of ab
+ * Note that a and b may be very large! 
+ * 
+ */
+
+using System.Numerics;
+
+
+namespace CodewarsKatas._5_kyu.Last_digit_of_a_large_number
+{
+    public class LastDigitOfALargeNumber
+    {
+        public static int GetLastDigit(BigInteger n1, BigInteger n2)
+        {
+            BigInteger result = BigInteger.ModPow(n1, n2, 10);
+
+            return (int)result;
+        }
+    }
+}
