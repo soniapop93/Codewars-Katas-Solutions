8e6d71283c0e98c010b8d975840056131d4b953c
soniapop93 <popsonia85@gmail.com>
2023-06-06 13:43:21 +0300

    finished challenge compare 2 digit numbers

---
+++ C#/CodewarsKatas/CodewarsKatas/7-kyu/Compare 2 digit numbers/Compare2DigitNumbers.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodewarsKatas._7_kyu.Compare_2_digit_numbers
+{
+    public class Compare2DigitNumbers
+    {
+        public static string Compare(int a, int b)
+        {
+            int count = 0;
+            string x = "";
+
+            if (a == b)
+            {
+                return "100%";
+            }
+
+            foreach (char c in a.ToString())
+            {
+                if (b.ToString().Contains(c) && x != c.ToString())
+                {
+                    x = c.ToString();
+                    count++;
+                }
+            }
+
+            if (count == a.ToString().Length)
+            {
+                return "100%";
+            }
+            else if(count == 0)
+            {
+                return "0%";
+            }
+            else
+            {
+                return "50%";
+            }
+        }
+    }
+}
