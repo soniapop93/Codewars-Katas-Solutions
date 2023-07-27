4d0f9c86347a099b8402199d6d1576c7e2b815ef
soniapop93 <popsonia85@gmail.com>
2023-07-27 15:28:32 +0300

    finished 2 katas: two numbers are positive and help suzuki rake his garden

---
+++ C#/CodewarsKatas/CodewarsKatas/7-kyu/Help Suzuki rake his garden/HelpSuzukiRakeHisGarden.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * LINK: https://www.codewars.com/kata/571c1e847beb0a8f8900153d/train/csharp
+ * 
+ * Help Suzuki rake his garden!
+ * 
+ * The monastery has a magnificent Zen garden made of white gravel and rocks and it is raked diligently everyday by the monks. 
+ * Suzuki having a keen eye is always on the lookout for anything creeping into the garden that must be removed during the daily raking such as insects or moss.
+ * 
+ * You will be given a string representing the garden such as:
+ * 
+ * garden = 'gravel gravel gravel gravel gravel gravel gravel gravel gravel rock slug ant gravel gravel snail rock gravel gravel 
+ * gravel gravel gravel gravel gravel slug gravel ant gravel gravel gravel gravel rock slug gravel gravel gravel gravel gravel snail gravel gravel 
+ * rock gravel snail slug gravel gravel spider gravel gravel gravel gravel gravel gravel gravel gravel moss gravel gravel gravel snail gravel gravel gravel ant gravel gravel moss gravel gravel gravel gravel snail gravel gravel gravel gravel slug gravel rock gravel gravel rock gravel gravel gravel gravel snail gravel gravel rock gravel gravel gravel gravel gravel spider gravel rock gravel gravel'
+ * Rake out any items that are not a rock or gravel and replace them with gravel such that:
+ * 
+ * garden = 'slug spider rock gravel gravel gravel gravel gravel gravel gravel'
+ * Returns a string with all items except a rock or gravel replaced with gravel:
+ * 
+ * garden = 'gravel gravel rock gravel gravel gravel gravel gravel gravel gravel'
+ * 
+ * 
+ */
+
+using System.Collections.Generic;
+
+namespace CodewarsKatas._7_kyu.Help_Suzuki_rake_his_garden
+{
+    public class HelpSuzukiRakeHisGarden
+    {
+        public static string RakeGarden(string garden)
+        {
+            string[] gardenArray = garden.Split(" ");
+            List<string> result = new List<string>();
+
+            for (int i = 0; i < gardenArray.Length; i++)
+            {
+                if (!gardenArray[i].Equals("rock") && !gardenArray[i].Equals("gravel"))
+                {
+                    result.Add("gravel");
+                }
+                else
+                {
+                    result.Add(gardenArray[i]);
+                }
+            }
+
+            return String.Join(" ", result);
+        }
+    }
+}
---
+++ C#/CodewarsKatas/CodewarsKatas/7-kyu/Two numbers are positive/TwoNumbersArePositive.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * LINK: https://www.codewars.com/kata/602db3215c22df000e8544f0/train/csharp
+ * 
+ * Your job is to write a function, which takes three integers a, b, and c as arguments, and returns 
+ * True if exactly two of of the three integers are positive numbers (greater than zero), and False - otherwise.
+ */
+
+namespace CodewarsKatas._7_kyu.Two_numbers_are_positive
+{
+    public class TwoNumbersArePositive
+    {
+        public static bool TwoArePositive(int a, int b, int c)
+        {
+            int count = 0;
+
+            if (a > 0)
+            {
+                count++;
+            }
+            if (b > 0)
+            {
+                count++;                
+            }
+            if (c > 0)
+            {
+                count++;
+            }
+
+            if (count == 2)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
