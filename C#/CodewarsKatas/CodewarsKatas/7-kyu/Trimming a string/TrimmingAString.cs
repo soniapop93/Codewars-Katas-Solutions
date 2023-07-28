039f5bb7a5486554542e200d0d7d4578da994846
soniapop93 <popsonia85@gmail.com>
2023-07-28 13:08:06 +0300

    finished 2 katas: trimming a string and vowel count

---
+++ C#/CodewarsKatas/CodewarsKatas/7-kyu/Trimming a string/TrimmingAString.cs
@@ -0,0 +1,81 @@
+﻿/*
+ * LINK: https://www.codewars.com/kata/563fb342f47611dae800003c/train/csharp
+ * Create a function that will trim a string (the first argument given) if it is longer than the requested maximum string length 
+ * (the second argument given). 
+ * The result should also end with "..."
+ * 
+ * These dots at the end also add to the string length.
+ * 
+ * For example, trim("Creating kata is fun", 14) should return "Creating ka..."
+ * 
+ * If the string is smaller or equal than the maximum string length, then simply return the string with no trimming or dots required.
+ * 
+ * e.g. trim("Code Wars is pretty rad", 50) should return "Code Wars is pretty rad"
+ * 
+ * If the requested string length is smaller than or equal to 3 characters, then the length of the dots is not added to the string length.
+ * 
+ * e.g. trim("He", 1) should return "H...", because 1 <= 3
+ * 
+ * Requested maximum length will be greater than 0. Input string will not be empty.
+ * 
+ */
+
+namespace CodewarsKatas._7_kyu.Trimming_a_string
+{
+    public class TrimmingAString
+    {
+        public static string TrimString(string phrase, int len)
+        {
+            string newStr = "";
+
+            if (phrase.Length <= 3)
+            {
+                int count = 0;
+
+                if (len > phrase.Length)
+                {
+                    count = phrase.Length;
+                }
+                else
+                {
+                    count = len;
+                }
+
+                for (int i = 0; i < count; i++)
+                {
+                    newStr += phrase[i];
+                }
+                if (count != 3 && count != phrase.Length)
+                {
+                    newStr += "...";
+                }
+            }
+
+            else if (phrase.Length > len)
+            {
+                int count = 0;
+
+                if (len > 3)
+                {
+                    count = len - 3;
+                }
+                else
+                {
+                    count = len;
+                }
+                for (int i = 0; i < count; i++)
+                {
+                    newStr += phrase[i];
+                }
+                newStr += "...";
+            }
+
+            else if (phrase.Length <= len)
+            {
+                newStr = phrase;
+            }
+
+            return newStr;
+        }
+    }
+}
---
+++ C#/CodewarsKatas/CodewarsKatas/7-kyu/Vowel Count/VowelCount.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * LINK: https://www.codewars.com/kata/54ff3102c1bad923760001f3/train/csharp
+ * 
+ * Return the number (count) of vowels in the given string.
+ *  
+ * We will consider a, e, i, o, u as vowels for this Kata (but not y).
+ * 
+ * The input string will only consist of lower case letters and/or spaces.
+ */
+
+namespace CodewarsKatas._7_kyu.Vowel_Count
+{
+    public class VowelCount
+    {
+        public static int GetVowelCount(string str)
+        {
+            int vowelCount = 0;
+
+            List<string> vowels = new List<string>() { "a", "e", "i", "o", "u"};
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (vowels.Contains(str[i].ToString()))
+                {
+                    vowelCount++;
+                }
+            }
+
+            return vowelCount;
+        }
+    }
+}
