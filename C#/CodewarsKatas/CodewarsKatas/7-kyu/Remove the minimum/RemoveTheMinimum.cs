8397d2aaaa6be40135494b4fc30f6445f8db1eae
soniapop93 <popsonia85@gmail.com>
2023-08-08 11:04:02 +0300

    finished 2 katas, remove the minimum & reverse words

---
+++ C#/CodewarsKatas/CodewarsKatas/7-kyu/Remove the minimum/RemoveTheMinimum.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * LINK: https://www.codewars.com/kata/563cf89eb4747c5fb100001b/train/csharp
+ * 
+ * Given an array of integers, remove the smallest value. Do not mutate the original array/list.
+ * If there are multiple elements with the same value, remove the one with a lower index. 
+ * If you get an empty array/list, return an empty array/list.
+ * 
+ * Don't change the order of the elements that are left.
+ * 
+ * Examples
+    * Input: [1,2,3,4,5], output = [2,3,4,5]
+    * Input: [5,3,2,1,4], output = [5,3,2,4]
+    * Input: [2,2,1,2,1], output = [2,2,2,1]
+ */
+
+namespace CodewarsKatas._7_kyu.Remove_the_minimum
+{
+    public class RemoveTheMinimum
+    {
+        public static List<int> RemoveSmallest(List<int> numbers)
+        {
+            List<int> result = new List<int>();
+
+            int lowest = int.MaxValue;
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] < lowest)
+                {
+                    lowest = numbers[i];
+                }
+            }
+
+            bool identified = false;
+            
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == lowest && !identified)
+                {
+                    identified = true;
+                    continue;
+                }
+                result.Add(numbers[i]);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ C#/CodewarsKatas/CodewarsKatas/7-kyu/Reverse Words/ReverseWordsKata.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * LINK: https://www.codewars.com/kata/5259b20d6021e9e14c0010d4/train/csharp
+ * 
+ * Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+ * 
+ * Examples
+ * "This is an example!" ==> "sihT si na !elpmaxe"
+ * "double  spaces"      ==> "elbuod  secaps"
+ */
+
+namespace CodewarsKatas._7_kyu.Reverse_Words
+{
+    public class ReverseWordsKata
+    {
+        public static string ReverseWords(string str)
+        {
+            string result = "";
+
+            string[] splittedStr = str.Split(" ");
+
+            for (int i = 0; i < splittedStr.Length; i++)
+            {
+                string temp = "";
+
+                for (int j = splittedStr[i].Length - 1; j >= 0; j-- )
+                {
+                    temp += splittedStr[i][j];
+                }
+
+                result += temp + " ";
+            }
+
+            return result.Substring(0, result.Length - 1);
+        }
+    }
+}
