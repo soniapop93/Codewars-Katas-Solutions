314b60db109c75dc5e30303b400d1797a83636f8
soniapop93 <popsonia85@gmail.com>
2023-08-15 16:06:33 +0300

    finished kata split strings

---
+++ C#/CodewarsKatas/CodewarsKatas/6-kyu/Split Strings/SplitStrings.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * LINK: https://www.codewars.com/kata/515de9ae9dcfc28eb6000001/train/csharp
+ * 
+ * Complete the solution so that it splits the string into pairs of two characters. 
+ * If the string contains an odd number of characters then it should replace the missing second 
+ * character of the final pair with an underscore ('_').
+ * 
+ * Examples:
+ * 
+ * 'abc' =>  ['ab', 'c_']
+ * 'abcdef' => ['ab', 'cd', 'ef']
+ */
+
+using System.Collections.Generic;
+
+namespace CodewarsKatas._6_kyu.Split_Strings
+{
+    public class SplitStrings
+    {
+        public static string[] Solution(string str)
+        {
+            List<string> result = new List<string>();
+
+            for (int i = 0; i < str.Length; i+=2)
+            {
+                string item = "";
+
+                if (i + 1 < str.Length)
+                {
+                    item = str[i].ToString() + str[i + 1];
+                }
+                else
+                {
+                    item = str[i].ToString() + "_";
+                }
+                result.Add(item);
+            }
+
+            return result.ToArray();
+        }
+    }
+}
