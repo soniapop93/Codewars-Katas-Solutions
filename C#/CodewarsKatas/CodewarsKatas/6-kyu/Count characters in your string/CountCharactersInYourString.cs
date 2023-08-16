3a26ecce443d15a87aaf1e481c01f80854e2c36d
soniapop93 <popsonia85@gmail.com>
2023-08-16 21:54:54 +0300

    finished kata count chars in your string

---
+++ C#/CodewarsKatas/CodewarsKatas/6-kyu/Count characters in your string/CountCharactersInYourString.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * LINK: https://www.codewars.com/kata/52efefcbcdf57161d4000091/train/csharp
+ * 
+ * The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+ * 
+ * What if the string is empty? Then the result should be empty object literal, {}.
+ * 
+ */
+
+
+namespace CodewarsKatas._6_kyu.Count_characters_in_your_string
+{
+    public class CountCharactersInYourString
+    {
+        public static Dictionary<char, int> Count(string str)
+        {
+            Dictionary<char, int> result = new Dictionary<char, int>();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (result.ContainsKey(str[i]))
+                {
+                    result[str[i]] = result[str[i]] + 1;
+                }
+                else
+                {
+                    result[str[i]] = 1;
+                }
+            }
+
+            return result;
+        }
+    }
+}
