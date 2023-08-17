3e7ddae17d0a5f675363783627bdbf4be77b885b
soniapop93 <popsonia85@gmail.com>
2023-08-17 22:14:40 +0300

    finished kata extract the file name. solved using regex

---
+++ C#/CodewarsKatas/CodewarsKatas/6-kyu/Extract the file name/ExtractTheFileName.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * LINK: https://www.codewars.com/kata/597770e98b4b340e5b000071/train/csharp
+ * 
+ * You have to extract a portion of the file name as follows:
+ * 
+ * Assume it will start with date represented as long number
+ * Followed by an underscore
+ * You'll have then a filename with an extension
+ * it will always have an extra extension at the end
+ * Inputs:
+ * 1231231223123131_FILE_NAME.EXTENSION.OTHEREXTENSION
+ * 
+ * 1_This_is_an_otherExample.mpg.OTHEREXTENSIONadasdassdassds34
+ * 
+ * 1231231223123131_myFile.tar.gz2
+ * Outputs
+ * FILE_NAME.EXTENSION
+ * 
+ * This_is_an_otherExample.mpg
+ * 
+ * myFile.tar
+ * Acceptable characters for random tests:
+ * 
+ * abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-0123456789
+ * 
+ * The recommended way to solve it is using RegEx and specifically groups.
+ * 
+ */
+
+using System.Text.RegularExpressions;
+
+namespace CodewarsKatas._6_kyu.Extract_the_file_name
+{
+    public class ExtractTheFileName
+    {
+        public static string ExtractFileName(string dirtFileName)
+        {
+            string pattern = @"_(.*)\.";
+            Regex reg = new Regex(pattern, RegexOptions.IgnoreCase);
+            Match matchReg = reg.Match(dirtFileName);
+
+            return matchReg.Groups[1].ToString();
+        }
+    }
+}
