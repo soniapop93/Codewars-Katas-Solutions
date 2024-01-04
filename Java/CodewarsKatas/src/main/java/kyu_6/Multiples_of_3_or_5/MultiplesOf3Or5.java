f8e63ee12b3b07d1225bc6f5e5a095a658ca3332
soniapop93 <popsonia85@gmail.com>
2024-01-04 13:11:27 +0200

    added the link to the last kata - java

--- Java/CodewarsKatas/src/main/java/kyu_6/Multiples_of_3_or_5/MultiplesOf3Or5.java
+++ Java/CodewarsKatas/src/main/java/kyu_6/Multiples_of_3_or_5/MultiplesOf3Or5.java
@@ -1,3 +1,15 @@
+/*
+LINK: https://www.codewars.com/kata/514b92a657cdc65150000006/train/java
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5,
+we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Finish the solution so that it returns the sum of all the multiples of 3 or
+ 5 below the number passed in.
+
+Note: If the number is a multiple of both 3 and 5, only count it once.
+ */
+
 package kyu_6.Multiples_of_3_or_5;
 
 public class MultiplesOf3Or5 {
